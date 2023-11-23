3b3d746528ad3f4ec6daf870f9bf3000004c51a5
leachristy <139340127+leachristy@users.noreply.github.com>
2023-11-23 15:01:29 -0800

    Update fetchAPI.js

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,26 +1,41 @@
 import React, { useState, useEffect } from "react";
 
+// Function to decode HTML entities
+function decodeString(str) {
+  const textArea = document.createElement("textarea");
+  textArea.innerHTML = str;
+  return textArea.value;
+}
+
 export default function FetchApi() {
   const [questions, setQuestions] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const getTrivia = async () => {
-      try {
-        const response = await fetch("https://opentdb.com/api.php?amount=10&type=multiple");
-        const json = await response.json();
-        console.log(json);
-        setQuestions(json.results);
-      } catch (error) {
-        console.error("Error fetching trivia:", error);
-        setError("Error fetching trivia. Please try again later.");
-      } finally {
-        setLoading(false);
-      }
-    };
+  const fetchRandomTrivia = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://opentdb.com/api.php?amount=10&type=multiple"
+      );
+      const json = await response.json();
+      // Decode HTML entities in questions and correct answers
+      const decodedData = json.results.map((item) => ({
+        question: decodeString(item.question),
+        correct_answer: decodeString(item.correct_answer),
+      }));
+      setQuestions(decodedData);
+    } catch (error) {
+      console.error("Error fetching trivia:", error);
+      setError("Error fetching trivia. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    getTrivia();
+  useEffect(() => {
+    fetchRandomTrivia();
   }, []); // Empty dependency array to run the effect only once
 
   const Questions = () => {
@@ -36,12 +51,17 @@ export default function FetchApi() {
       <ul>
         {questions.map((item) => (
           <li key={item.question}>
-            Question: {item.question} / Answer: {item.correct_answer}
+            Question: {item.question} <br /> Answer: {item.correct_answer}
           </li>
         ))}
       </ul>
     );
   };
 
-  return <Questions />;
+  return (
+    <div>
+      <button onClick={fetchRandomTrivia}>Generate Random Questions</button>
+      <Questions />
+    </div>
+  );
 }
